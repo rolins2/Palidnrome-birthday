54d1f12767f4f0a3edc3871892028e152c2280fe
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-07-19 16:03:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const output = document.querySelector("#output");
 
 submitbtn.addEventListener("click", function clickEventHandler(){
     console.log("Button Clicked");
-    var d = {day:29,
+    var d = {day:5,
             month:2,
-                year:2023};
-                dateToString(d)
+                year:2020};
+                
+
+                if(checkPalindromeForAllForats(d)){
+                    console.log("Is palindrome");
+                }
+                else{
+                    console.log("Not a palindrme");
+                }
     
 })
 
@@ -38,13 +45,13 @@ function reverseString(str){
 
 function checkPalindrome(date){
     var rev = reverseString(date);
-    console.log(rev);
+    
     if(rev === date){
-        console.log("Is Palindrome");
+        
         return true;
     }
     else{
-        console.log("Not palindrome");
+        
         return false;
     }
 
@@ -65,8 +72,40 @@ function dateToString(date){
     }
     dateInstr.year = date.year.toString();
 
-
-    console.log(dateInstr);
-
      return dateInstr;
+}
+
+function getAllDateFormats(date){
+    var dateInStr = dateToString(date);
+     var ddmmyyyy = dateInStr.day + dateInStr.month + dateInStr.year;
+     var mmddyyyy = dateInStr.month + dateInStr.day + dateInStr.year;
+    var yyyymmdd = dateInStr.year + dateInStr.month + dateInStr.day;
+    var ddmmyy = dateInStr.day + dateInStr.month + dateInStr.year.slice(-2);
+    var mmddyy = dateInStr.month + dateInStr.day+ dateInStr.year.slice(-2);
+    var yymmdd = dateInStr.year.slice(-2) + dateInStr.month + dateInStr.day;
+   
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
+
+function checkPalindromeForAllForats(date){
+    var arr = getAllDateFormats(date);
+ 
+
+    var flag = false;
+    for(var i=0; i < arr.length;i++){
+        
+        if(checkPalindrome(arr[i])){
+
+
+
+            flag = true;
+            break;
+
+        }
+        
+        
+
+    }
+    return flag;
 }
